7d7bdd700e042a76e15d8611e1f2e72408500b4f
thejessleigh <jessunrein@Jesss-MacBook-Pro.local>
2019-10-16 16:20:36 -0500

    Add seasons to friday.

--- js/friday.js
+++ js/friday.js
@@ -1,11 +1,25 @@
 function onLoad() {
-    var today = new Date().getDay();
-    let element = document.getElementById("friday")
-    if (today === 5) {
-        element.innerHTML = "YES";
+    let today = new Date().getDay();
+    let element = document.getElementById("friday");
+    if (today === 3) {
+      element.innerHTML = "YES";
+      let month = new Date().getMonth();
+
+      season = "";
+      switch (true) {
+        case month >= 3 && month < 6:
+          season = "spring";
+        case month >= 6 && month < 9:
+          season = "summer";
+        case month >= 9 && month < 12:
+          season = "autumn";
+        default:
+          season = "winter";
+      }
+      document.body.classList.add(season);
     } else {
-        element.innerHTML = "NO";
+      element.innerHTML = "NO";
     }
-}
+  }
 
 onLoad();
